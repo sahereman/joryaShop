d1b37e856cc79406135f7cbe55eeed5a113bf345
Yxq <15610538128@163.com>
2018-11-06 17:38:50 +0800

    选择支付页面倒计时

--- resources/views/orders/payment_method.blade.php
+++ resources/views/orders/payment_method.blade.php
@@ -45,7 +45,31 @@
 @section('scriptsAfterJs')
     <script type="text/javascript">
         $(function () {
-        	
+        	//倒计时方法封装
+            function timeCount(remain_id, start_time, ending_time, type) {
+                function _fresh() {
+                    var nowDate = new Date(); //当前时间
+                    var id = $('#' + remain_id).attr("order_id"); //当前订单的id
+                    var addTime = new Date(parseInt(start_time));               //返回的时间戳转换成时间格式
+                    var auto_totalS = ending_time; //订单支付有效时长
+                    var ad_totalS = parseInt((addTime.getTime() / 1000) + auto_totalS); ///下单总秒数
+                    var totalS = parseInt(ad_totalS - (nowDate.getTime() / 1000)); ///支付时长
+                    if (totalS > 0) {
+                        var _day = parseInt((totalS / 3600) % 24 / 24);
+                        var _hour = parseInt((totalS / 3600) % 24);
+                        var _minute = parseInt((totalS / 60) % 60);
+                        var _second = parseInt(totalS % 60);
+                        if (type == '1') {
+                            $('#' + remain_id).html('剩余' + _hour + '时' + _minute + '分' + _second + '秒支付（若超时未支付订单，系统将自动取消订单）');
+                        } else {
+                            $('#' + remain_id).html('剩余' + _day + '天' + _hour + '时' + _minute + '分确认（若超时未确认订单，系统将自动确认订单）');
+                        }
+                    }
+                }
+
+                _fresh();
+                var sh = setInterval(_fresh, 1000);
+            }
         });
     </script>
 @endsection
