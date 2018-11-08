26d5548f29d980aa92e9f3b40ca3b7b39ba1c0c2
Miracle <120007700@qq.com>
2018-11-08 19:10:32 +0800

    mobile - 重置密码

---
+++ resources/views/mobile/index/root.bak.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.mobile')
+
+@section('content')
+    {{--如果需要引入子视图--}}
+    {{--@include('layouts._header')--}}
+
+    {{--填充页面内容--}}
+    <h1>首页</h1>
+
+
+
+    @guest
+        <a href="{{route('mobile.login.show')}}">登录</a>
+
+        <a href="{{ route('mobile.register.show') }}">注册</a>
+
+        <a href="{{ route('mobile.reset.sms.show') }}">忘记密码?</a>
+
+    @else
+        <h2>用户名: {{Auth::user()->name}}</h2>
+
+        <a href="{{ route('mobile.logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+        <form id="logout-form" action="{{ route('mobile.logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+        </form>
+    @endguest
+
+
+
+    {{--如果需要引入子视图--}}
+    {{--@include('layouts._footer')--}}
+@endsection
+
+
+@section('scriptsAfterJs')
+    <script type="text/javascript">
+        //页面单独JS写这里
+    </script>
+@endsection
