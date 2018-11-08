f379b8d5931bfe8b56adffdc61c1aa1b472cb01d
hmaisky <lhm17863972036@163.com>
2018-11-08 19:09:00 +0800

    雅美移动端商城首页编写

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Web 标题')</title>
@@ -11,7 +11,7 @@
 
     <!-- 样式 -->
     <link href="{{ asset('static_m/css/app.css') }}" rel="stylesheet">
-    {{--<link href="{{ asset('js/swiper/css/swiper.css') }}" rel="stylesheet">--}}
+    <link href="{{ asset('js/swiper/css/swiper_m.min.css') }}" rel="stylesheet">
 
 </head>
 <body>
@@ -21,7 +21,7 @@
 </div>
 <!-- JS 脚本 -->
 <script src="{{ asset('static_m/js/app.js') }}"></script>
-{{--<script src="{{ asset('js/layer/layer.js') }}"></script>--}}
+<script src="{{ asset('js/swiper/js/swiper_m.min.js') }}"></script>
 
 
 @yield('scriptsAfterJs')
--- resources/views/mobile/index/root.blade.php
+++ resources/views/mobile/index/root.blade.php
@@ -1,21 +1,65 @@
 @extends('layouts.mobile')
-
+@section('title', '卓雅美业')
 @section('content')
+	<div class="searchBox">
+		<div class="searchCon">
+			<img src="{{ asset('static_m/img/Unchecked_search.png') }}"/>
+			<input type="text" name="" id="" value="" placeholder="搜索商品，供12351款好货"/>
+		</div>
+	</div>
+	<!-- Swiper -->
+  <div class="swiper-container" >		
+	  <div class="swiper-wrapper">
+	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
+	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
+	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
+	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
+	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
+	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
+	  </div>
+	  <div class="swiper-pagination" id="pagination"></div>
+	</div>
+	 
     {{--如果需要引入子视图--}}
-    {{--@include('layouts._header')--}}
-
-    {{--填充页面内容--}}
-    首页
-
-
-
-    {{--如果需要引入子视图--}}
-    {{--@include('layouts._footer')--}}
+    @include('layouts._footer_mobil')
 @endsection
 
 
 @section('scriptsAfterJs')
     <script type="text/javascript">
         //页面单独JS写这里
+    var mySwiper = new Swiper('.swiper-container',{
+	  slidesPerView : 'auto',
+	  centeredSlides : true,
+	  watchSlidesProgress: true,
+	  pagination : '.swiper-pagination',
+	  paginationClickable: true,
+      paginationBulletRender: function (index, className) {
+			return '<span class="' + className + '"><i></i></span>';
+      },
+	  onProgress: function(swiper){
+        for (var i = 0; i < swiper.slides.length; i++){
+          var slide = swiper.slides[i];
+          var progress = slide.progress;
+		  scale = 1 - Math.min(Math.abs(progress * 0.2), 1);
+        
+         es = slide.style;
+		 es.opacity = 1 - Math.min(Math.abs(progress/2),1);
+				es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = 'translate3d(0px,0,'+(-Math.abs(progress*150))+'px)';
+
+        }
+      },
+
+     onSetTransition: function(swiper, speed) {
+      	for (var i = 0; i < swiper.slides.length; i++) {
+				es = swiper.slides[i].style;
+				es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = speed + 'ms';
+		}
+
+      }
+  });
+
+        
+        
     </script>
 @endsection
