72fccc26d1a35ab4ed7605520a4c4b7ab7e0fcbd
hmaisky <lhm17863972036@163.com>
2018-11-09 18:22:20 +0800

    雅美移动端商城首页样式编写

--- resources/views/layouts/_footer_mobil.blade.php
+++ resources/views/layouts/_footer_mobil.blade.php
@@ -1,3 +1,55 @@
-<div>
-	<!--底部-->
+<div class="footer">
+	<div class="itemsF itemsS itemsActive">
+		<img src="{{ asset('static_m/img/Select_home.png') }}"/>
+		<span>首页</span>
+	</div>
+	<div class="itemsF itemsL">
+		<img src="{{ asset('static_m/img/Unchecked_classification.png') }}"/>
+		<span>分类</span>
+	</div>
+	<div class="itemsF itemsG">
+		<img src="{{ asset('static_m/img/Unchecked_Shopping.png') }}"/>
+		<span>购物车</span>
+	</div>
+	<div class="itemsF itemsW">
+		<img src="{{ asset('static_m/img/Unchecked_my.png') }}"/>
+		<span>我</span>
+	</div>
 </div>
+
+<script src="{{ asset('static_m/js/app.js') }}"></script>
+<script type="text/javascript">
+	$(".itemsS").on("click",function(){
+		$(".itemsF").removeClass("itemsActive");
+		$(this).addClass("itemsActive");
+		$(".itemsS img").attr("src","{{ asset('static_m/img/Select_home.png') }}");
+		$(".itemsL img").attr("src","{{ asset('static_m/img/Unchecked_classification.png') }}");
+		$(".itemsG img").attr("src","{{ asset('static_m/img/Unchecked_Shopping.png') }}");
+		$(".itemsW img").attr("src","{{ asset('static_m/img/Unchecked_my.png') }}");
+		window.location.href = "{{ route('mobile.root')}}";
+	});
+	$(".itemsL").on("click",function(){
+		$(".itemsF").removeClass("itemsActive");
+		$(this).addClass("itemsActive");
+		$(".itemsS img").attr("src","{{ asset('static_m/img/Unchecked_home.png') }}");
+		$(".itemsL img").attr("src","{{ asset('static_m/img/Select_classification.png') }}");
+		$(".itemsG img").attr("src","{{ asset('static_m/img/Unchecked_Shopping.png') }}");
+		$(".itemsW img").attr("src","{{ asset('static_m/img/Unchecked_my.png') }}");
+	});
+	$(".itemsG").on("click",function(){
+		$(".itemsF").removeClass("itemsActive");
+		$(this).addClass("itemsActive");
+		$(".itemsS img").attr("src","{{ asset('static_m/img/Unchecked_home.png') }}");
+		$(".itemsL img").attr("src","{{ asset('static_m/img/Unchecked_classification.png') }}");
+		$(".itemsG img").attr("src","{{ asset('static_m/img/Select_Shopping.png') }}");
+		$(".itemsW img").attr("src","{{ asset('static_m/img/Unchecked_my.png') }}");
+	});
+	$(".itemsW").on("click",function(){
+		$(".itemsF").removeClass("itemsActive");
+		$(this).addClass("itemsActive");
+		$(".itemsS img").attr("src","{{ asset('static_m/img/Unchecked_home.png') }}");
+		$(".itemsL img").attr("src","{{ asset('static_m/img/Unchecked_classification.png') }}");
+		$(".itemsG img").attr("src","{{ asset('static_m/img/Unchecked_Shopping.png') }}");
+		$(".itemsW img").attr("src","{{ asset('static_m/img/Select_my.png') }}");
+	});
+</script>
--- resources/views/mobile/index/root.blade.php
+++ resources/views/mobile/index/root.blade.php
@@ -1,34 +1,113 @@
 @extends('layouts.mobile')
 @section('title', '卓雅美业')
 @section('content')
-	<div class="searchBox">
-		<div class="searchCon">
-			<img src="{{ asset('static_m/img/Unchecked_search.png') }}"/>
-			<input type="text" name="" id="" value="" placeholder="搜索商品，供12351款好货"/>
+	<div class="main">
+		<div class="searchBox">
+			<div class="searchCon">
+				<img src="{{ asset('static_m/img/Unchecked_search.png') }}"/>
+				<input type="text" name="" id="" value="" placeholder="搜索商品，供12351款好货"/>
+			</div>
 		</div>
-	</div>
-	<!-- Swiper -->
-  <div class="swiper-container" >		
-	  <div class="swiper-wrapper">
-	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
-	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
-	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
-	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
-	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
-	    <div class="swiper-slide"><img src="{{ asset('static_m/img/banner.png') }}" class="main-img"></div>
-	  </div>
-	  <div class="swiper-pagination" id="pagination"></div>
+		<!-- Swiper -->
+	    <div class="swiper-container swiper-containerL" >		
+		  <div class="swiper-wrapper">
+		  	@for($i = 0; $i < 5; $i++)
+		    <div class="swiper-slide swiper-slideL">
+		    	<img src="{{ asset('static_m/img/banner.png') }}" class="main-img">
+		    </div>
+		    @endfor
+		  </div>
+		  <div class="swiper-pagination" id="pagination"></div>
+		</div>
+		<div class="proBox">
+			<div class="new_pro">
+				<div class="new_title">
+					<img src="{{ asset('static_m/img/Title_New.png') }}"/>
+					<span class="new_name">新品首发</span>
+				</div>
+				<div class="swiper-container swiper-containers">
+				    <div class="swiper-wrapper">
+				    @for($i = 0; $i < 6; $i++)
+				      <div class="swiper-slide swiper-slides">
+				      	<img src="{{ asset('static_m/img/new.png') }}"/>
+				      	<div class="new_pro_name">糖果色片染 十足立体感糖果色片染 十足立体感</div>
+				      	<span class="new_pro_price">￥129</span>
+				      </div>
+				     @endfor 
+				    </div>
+				  </div>
+			</div>
+			<div class="block_trend">
+				<div class="block_title">
+					<span>时尚趋势</span>
+					<a href="#">更多></a>
+				</div>
+				<img src="{{ asset('static_m/img/theme.png') }}" class="block_theme"/>
+				<div class="blockBox">
+					@for($i = 0; $i < 6; $i++)
+					<div class="blockItem">
+						<img src="{{ asset('static_m/img/blockImg.png') }}"/>
+						<div class="block_name">糖果色片染立体感十足,糖果色片</div>
+						<span class="block_price">￥129</span>
+					</div>
+					@endfor
+				</div>
+			</div>
+			<div class="block_trend">
+				<div class="block_title">
+					<span>高级定制</span>
+					<a href="#">更多></a>
+				</div>
+				<img src="{{ asset('static_m/img/Advancedcustomization.png') }}" class="block_theme"/>
+				<div class="cusBox">
+					<div class="cusItemO">
+						<img src="{{ asset('static_m/img/Advancedcustomization_02.png') }}"/>
+					</div>
+					<div class="cusItemF">
+						<img src="{{ asset('static_m/img/Advancedcustomization_03.png') }}"  />
+						<div class="cusItemBox">
+							<img src="{{ asset('static_m/img/blockImg.png') }}"/>
+							<img src="{{ asset('static_m/img/blockImg.png') }}"/>
+						</div>
+					</div>
+				</div>
+				<div class="blockBox">
+					@for($i = 0; $i < 6; $i++)
+					<div class="blockItem blockItemCus">
+						<img src="{{ asset('static_m/img/blockImg.png') }}"/>
+						<div class="block_name">糖果色片染</div>
+						<span class="block_price">￥129</span>
+					</div>
+					@endfor
+				</div>
+			</div>
+			<div class="pro_rec ">
+				<div class="new_title">
+					<img src="{{ asset('static_m/img/Title_Like.png') }}"/>
+					<span class="new_name">商品推荐</span>
+				</div>
+				<div class="recBox">
+					@for($i = 0; $i < 6; $i++)
+						<div class="recItem">
+							<img src="{{ asset('static_m/img/blockImg.png') }}"/>
+							<div class="block_name">糖果色片染立体感十足</div>
+							<span class="block_price">￥129</span>
+						</div>
+					@endfor
+				</div>
+			</div>
+		</div>
+		{{--如果需要引入子视图--}}
+    @include('layouts._footer_mobil')
 	</div>
 	 
-    {{--如果需要引入子视图--}}
-    @include('layouts._footer_mobil')
+    
 @endsection
 
 
 @section('scriptsAfterJs')
     <script type="text/javascript">
-        //页面单独JS写这里
-    var mySwiper = new Swiper('.swiper-container',{
+    var mySwiper = new Swiper('.swiper-containerL',{
 	  slidesPerView : 'auto',
 	  centeredSlides : true,
 	  watchSlidesProgress: true,
@@ -58,7 +137,14 @@
 
       }
   });
-
+var swiper = new Swiper('.swiper-containers', {
+      slidesPerView: 2.7,
+      spaceBetween: 0,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
         
         
     </script>
