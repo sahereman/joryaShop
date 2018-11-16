23cba9443b63560a7ad226fe0eb1e0545d510671
hmaisky <lhm17863972036@163.com>
2018-11-16 09:13:14 +0800

    订单列表部分页面编写

--- resources/views/mobile/orders/index.blade.php
+++ resources/views/mobile/orders/index.blade.php
@@ -1,16 +1,43 @@
 @extends('layouts.mobile')
-
+@section('title', '我的订单')
 @section('content')
-    {{--如果需要引入子视图--}}
-    {{--@include('layouts._header')--}}
+	<div class="orderBox">
+		<div class="orderHeadTop">
+	    	<div class="headerBar">
+				<img src="{{ asset('static_m/img/icon_backtop.png') }}" class="backImg"/>
+				<span>我的订单</span>
+			</div>
+			<div class="orderHead">
+				<div class="orderActive">全部</div>
+				<div>待付款</div>
+				<div>待收货</div>
+				<div>待评价</div>
+				<div>售后</div>
+			</div>
+	    </div>
+		<div class="orderMain">
+			<div class="orderItem">
+				<div class="orderItemH">
+					<span>订单编号:45654645464</span>
+					<span class="orderItemState">待付款</span>
+				</div>
+				<div class="orderItemDetail">
+					<img src="{{ asset('static_m/img/blockImg.png') }}"/>
+					<div class="orderDal">
+						<!--<span>卓业美业长直假发片</span>
+						<span>颜色：黄</span>-->
+					</div>
+					<div class="orderPrice">
+						
+					</div>
+				</div>
+				<div class="orderItemTotle">
+					
+				</div>
+			</div>
+		</div>
+	</div>
 
-    {{--填充页面内容--}}
-    <h1>我的订单 列表</h1>
-
-
-
-    {{--如果需要引入子视图--}}
-    {{--@include('layouts._footer')--}}
 @endsection
 
 
