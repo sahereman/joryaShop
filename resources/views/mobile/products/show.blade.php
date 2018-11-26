cf8bd80c25b2b5514a1efd7f612899fd12e8cfa8
hmaisky <lhm17863972036@163.com>
2018-11-26 18:20:37 +0800

    商品列表页面编写，商品详情部分页面编写

--- resources/views/mobile/products/show.blade.php
+++ resources/views/mobile/products/show.blade.php
@@ -1,20 +1,31 @@
 @extends('layouts.mobile')
-
+@section('title', '商品详情')
 @section('content')
-    {{--如果需要引入子视图--}}
-    {{--@include('layouts._header')--}}
-
-    {{--填充页面内容--}}
-    <h1>商品详情 页面</h1>
-
-
-    {{--如果需要引入子视图--}}
-    {{--@include('layouts._footer')--}}
-@endsection
+	<div class="goodsDetailBox">
+		<div class="goodsSwiper swiper-container">
+		    <div class="swiper-wrapper">
+		        @for($i = 0; $i < 5; $i++)
+			    <div class="swiper-slide">
+			    	<img src="{{ asset('static_m/img/blockImg.png') }}">
+			    </div>
+			    @endfor
+		    </div>
+		    <!-- 如果需要分页器 -->
+		    <div class="swiper-pagination"></div>
+		</div>
+	</div>
 
 
 @section('scriptsAfterJs')
     <script type="text/javascript">
         //页面单独JS写这里
+        var mySwiper = new Swiper ('.swiper-container', {
+//		    loop: true,
+		    // 如果需要分页器
+		    pagination: '.swiper-pagination',
+//		    autoplay:3000,
+//		    stopOnLastSlide:true,
+		    
+		  }) 
     </script>
 @endsection
