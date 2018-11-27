e8493cdb6241e0f1b393c0b0ad4eb8ac2219d89d
elijah <1198331919@qq.com>
2018-11-27 09:27:28 +0800

    Uncomment \App\Http\Middleware\VerifyCsrfToken::class @app/Http/Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             // development mode --without csrf--
-            // \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
 
