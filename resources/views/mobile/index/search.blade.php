8399261934274034551c2fb0d352ee257eedd1c4
elijah <1198331919@qq.com>
2018-11-30 14:06:28 +0800

    mobile.search + mobile.products.search

--- resources/views/mobile/index/search.blade.php
+++ resources/views/mobile/index/search.blade.php
@@ -1,50 +1,54 @@
 @extends('layouts.mobile')
 @section('title', '商品搜索')
 @section('content')
-	<div class="seaBox">
-		<div class="headerBar">
-			<img src="{{ asset('static_m/img/icon_backtop.png') }}" class="backImg" onclick="javascript:history.back(-1);"/>
-			<span>搜索</span>
-		</div>
-		<div class="searchHead">
-			<div class="searchHeadMain">
-				<img src="{{ asset('static_m/img/icon_search3.png') }}" class="seaImg"/>
-				<input type="text" name="" id="ipt" value="" placeholder="搜你想搜" />
-				<img src="{{ asset('static_m/img/icon_closed4.png') }}" class="seaClosed"/>
-			</div>
-			<span>搜索</span>
-		</div>
-		<div class="searchMain">
-			<div class="searchNow">
-				<h5>最近搜索</h5>
-				<div class="searchNowBox">
-					@if(true)
-					<!--暂无搜索历史-->
-						<div>暂无搜索历史</div>
-					@else
-						@for($i = 0;$i<3; $i++)
-							<span>网页</span>
-						@endfor
-					@endif
-				</div>
-			</div>
-			<div class="searchNow">
-				<h5>热门搜索</h5>
-				<div class="searchNowBox">
-					@for($i = 0;$i<3; $i++)
-						<span>海报</span>
-					@endfor
-				</div>
-			</div>
-		</div>
-		<div class="searchResult">
-			@for($i = 0;$i<4; $i++)
-				<div class="searchResultItem">
-					黄色中长假发片
-				</div>
-			@endfor
-		</div>
-	</div>
+    <div class="seaBox">
+        <div class="headerBar">
+            <img src="{{ asset('static_m/img/icon_backtop.png') }}" class="backImg"
+                 onclick="javascript:history.back(-1);"/>
+            <span>搜索</span>
+        </div>
+        <div class="searchHead">
+            <div class="searchHeadMain">
+                <img src="{{ asset('static_m/img/icon_search3.png') }}" class="seaImg"/>
+                <input type="text" name="" id="ipt" value="" placeholder="搜你想搜"/>
+                <img src="{{ asset('static_m/img/icon_closed4.png') }}" class="seaClosed"/>
+            </div>
+            <span>搜索</span>
+        </div>
+        <div class="searchMain">
+            <div class="searchNow">
+                <h5>最近搜索</h5>
+                <div class="searchNowBox">
+                    {{-- TODO ... LocalStorage --}}
+                    @if(true)
+                            <!--暂无搜索历史-->
+                    <div>暂无搜索历史</div>
+                    @else
+                        {{-- TODO ... search_history --}}
+                        @for($i = 0;$i<3; $i++)
+                            <span>网页</span>
+                        @endfor
+                    @endif
+                </div>
+            </div>
+            <div class="searchNow">
+                <h5>热门搜索</h5>
+                <div class="searchNowBox">
+                    <a href="{{ route('mobile.products.search') . '?query=精品' }}"><span>精品</span></a>
+                    <a href="{{ route('mobile.products.search') . '?query=黄色假发' }}"><span>黄色假发</span></a>
+                    <a href="{{ route('mobile.products.search') . '?query=中长假发' }}"><span>中长假发</span></a>
+                </div>
+            </div>
+        </div>
+        <div class="searchResult">
+            {{-- TODO ... search_hint | search_history --}}
+            @for($i = 0;$i<4; $i++)
+                <div class="searchResultItem">
+                    黄色中长假发片
+                </div>
+            @endfor
+        </div>
+    </div>
 
 @endsection
 
@@ -52,31 +56,31 @@
 @section('scriptsAfterJs')
     <script type="text/javascript">
         //页面单独JS写这里
-        $(".seaClosed").on("click",function(){
-        	$("#ipt").val("");
+        $(".seaClosed").on("click", function () {
+            $("#ipt").val("");
         });
-        $("#ipt").on("focus",function(){
-        	$(".searchMain").css("display","none");
-        	$(".searchResult").css("display","block");
-        	$(".searchHead span").html("取消");
+        $("#ipt").on("focus", function () {
+            $(".searchMain").css("display", "none");
+            $(".searchResult").css("display", "block");
+            $(".searchHead span").html("取消");
         });
-        $(".searchHead span").on("click",function(){
-        	if($(this).html() == "取消"){
-        		$(".searchMain").css("display","block");
-        		$(".searchResult").css("display","none");
-        		$(this).html("搜索");
-        	}else{
-        		//点击搜索跳转商品列表页面TODO
-        		
-        	}
+        $(".searchHead span").on("click", function () {
+            if ($(this).html() == "取消") {
+                $(".searchMain").css("display", "block");
+                $(".searchResult").css("display", "none");
+                $(this).html("搜索");
+            } else {
+                //点击搜索跳转商品列表页面TODO
+
+            }
         });
-        $("#ipt").change(function(){
-        	var val = $(this).val();
-        	//将输入框的值传入后台，将接口返回的模糊搜索数据渲染到页面TODO
-        	
+        $("#ipt").change(function () {
+            var val = $(this).val();
+            //将输入框的值传入后台，将接口返回的模糊搜索数据渲染到页面TODO
+
         });
-        $(".searchResultItem").on("click",function(){
-        	window.location.href = "{{route('mobile.products.search')}}";
+        $(".searchResultItem").on("click", function () {
+            window.location.href = "{{route('mobile.products.search')}}";
         });
     </script>
 @endsection
--- resources/views/mobile/products/search.blade.php
+++ resources/views/mobile/products/search.blade.php
@@ -1,110 +1,110 @@
 @extends('layouts.mobile')
 @section('title', '商品列表')
 @section('content')
-	<div class="goodsListBox">
-		<div class="goodsListHead">
-			<div class="goodsListSearch">
-				<img src="{{ asset('static_m/img/icon_backtop.png') }}"  onclick="javascript:history.back(-1);"/>
-				<div class="goodsListHeadBox">
-					<img src="{{ asset('static_m/img/icon_search3.png') }}" class="searchImg"/>
-					<input type="text" name="" id="ipt" value="卷发" />
-					<!--<div class="searchCon">
-						<span>卷发</span>
-						<img src="{{ asset('static_m/img/icon_searchclosed.png') }}"/>
-					</div>-->
-				</div>
-			</div>
-			<div class="goodsListFillter">
-				<div class="zonghe fillterItem">
-					综合
-					<div class="liftingBox">
-						<span class="up">▴</span>
-						<span class="down">▾</span>
-					</div>
-				</div>
-				<div class="fillterItem">
-					销量
-					<span></span>
-				</div>
-				<div class="fillterItem">
-					价格
-					<div class="liftingBox">
-						<span class="up">▴</span>
-						<span class="down">▾</span>
-					</div>
-				</div>
-				<div class="fillterItem">
-					人气
-					<div class="liftingBox">
-						<span class="up">▴</span>
-						<span class="down">▾</span>
-					</div>
-				</div>
-				<div class="dropDownBox" name="isPull">
-					<div>
-						综合排序
-					</div>
-					<div>
-						新品优先
-					</div>
-					<div>
-						评论由高到低
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="goodsListMain">
-			@for($i = 0; $i< 14; $i++)
-				<div class="goodsListItem">
-					<img src="{{ asset('static_m/img/blockImg.png') }}" alt="" />
-					<div class="goodsItemBlock">
-						<div class="goodsBlockName">
-							糖果色片染十足立体感
-						</div>
-						<div class="goodsBlockPrice">
-							￥129
-						</div>
-					</div>
-				</div>
-			@endfor
-			
-		</div>
-	</div>
-@endsection
+    <div class="goodsListBox">
+        <div class="goodsListHead">
+            <div class="goodsListSearch">
+                <img src="{{ asset('static_m/img/icon_backtop.png') }}" onclick="javascript:history.back(-1);"/>
+                <div class="goodsListHeadBox">
+                    <img src="{{ asset('static_m/img/icon_search3.png') }}" class="searchImg"/>
+                    <input type="text" name="" id="ipt" value="卷发"/>
+                    {{--<div class="searchCon">
+                        <span>卷发</span>
+                        <img src="{{ asset('static_m/img/icon_searchclosed.png') }}"/>
+                    </div>--}}
+                </div>
+            </div>
+            <div class="goodsListFillter">
+                <div class="zonghe fillterItem">
+                    综合
+                    <div class="liftingBox">
+                        <span class="up">▴</span>
+                        <span class="down">▾</span>
+                    </div>
+                </div>
+                <div class="fillterItem">
+                    销量
+                    <span></span>
+                </div>
+                <div class="fillterItem">
+                    价格
+                    <div class="liftingBox">
+                        <span class="up">▴</span>
+                        <span class="down">▾</span>
+                    </div>
+                </div>
+                <div class="fillterItem">
+                    人气
+                    <div class="liftingBox">
+                        <span class="up">▴</span>
+                        <span class="down">▾</span>
+                    </div>
+                </div>
+                <div class="dropDownBox" name="isPull">
+                    <div>
+                        综合排序
+                    </div>
+                    <div>
+                        新品优先
+                    </div>
+                    <div>
+                        评论由高到低
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="goodsListMain">
+            {{-- TODO ... search_more --}}
+            @for($i = 0; $i< 14; $i++)
+                <div class="goodsListItem">
+                    <img src="{{ asset('static_m/img/blockImg.png') }}" alt=""/>
+                    <div class="goodsItemBlock">
+                        <div class="goodsBlockName">
+                            糖果色片染十足立体感
+                        </div>
+                        <div class="goodsBlockPrice">
+                            ￥129
+                        </div>
+                    </div>
+                </div>
+            @endfor
 
+        </div>
+    </div>
+@endsection
 
 @section('scriptsAfterJs')
     <script type="text/javascript">
         //页面单独JS写这里
-        $(".zonghe").on("click",function(){
-        	if($(".dropDownBox").attr("name")=="isPull"){
-        		$(".dropDownBox").attr("name","pull");
-        		$(".dropDownBox").slideDown();
-        		$(".zonghe .liftingBox .up").css("display","block");
-        		$(".zonghe .liftingBox .down").css("display","none");
-        	}else if($(".dropDownBox").attr("name")=="pull"){
-        		$(".dropDownBox").attr("name","isPull");
-        		$(".dropDownBox").slideUp();
-        		$(".zonghe .liftingBox .up").css("display","none");
-        		$(".zonghe .liftingBox .down").css("display","block");
-        	}
+        $(".zonghe").on("click", function () {
+            if ($(".dropDownBox").attr("name") == "isPull") {
+                $(".dropDownBox").attr("name", "pull");
+                $(".dropDownBox").slideDown();
+                $(".zonghe .liftingBox .up").css("display", "block");
+                $(".zonghe .liftingBox .down").css("display", "none");
+            } else if ($(".dropDownBox").attr("name") == "pull") {
+                $(".dropDownBox").attr("name", "isPull");
+                $(".dropDownBox").slideUp();
+                $(".zonghe .liftingBox .up").css("display", "none");
+                $(".zonghe .liftingBox .down").css("display", "block");
+            }
         });
-        $(".goodsListFillter .fillterItem").on("click",function(){
-        	$(".goodsListFillter div").removeClass("goodsFillterActive");
-        	$(this).addClass("goodsFillterActive");
-        	$(".dropDownBox").slideUp();
+        $(".goodsListFillter .fillterItem").on("click", function () {
+            $(".goodsListFillter div").removeClass("goodsFillterActive");
+            $(this).addClass("goodsFillterActive");
+            $(".dropDownBox").slideUp();
         });
-        $(".dropDownBox div").on("click",function(){
-        	$('.dropDownBox div').removeClass("goodsFillterActive");
-        	$(this).addClass("goodsFillterActive");
-        	$(".dropDownBox").slideUp();
+        $(".dropDownBox div").on("click", function () {
+            $('.dropDownBox div').removeClass("goodsFillterActive");
+            $(this).addClass("goodsFillterActive");
+            $(".dropDownBox").slideUp();
         });
-        $("#ipt").on("focus",function(){
-        	window.location.href = "{{route('mobile.search')}}";
+        $("#ipt").on("focus", function () {
+            window.location.href = "{{route('mobile.search')}}";
         });
-        $(".goodsListItem").on('click',function(){
-        	window.location.href = "{{route('mobile.products.show',60)}}";
+        $(".goodsListItem").on('click', function () {
+            window.location.href = "{{route('mobile.products.show',60)}}";
         });
-        
+
     </script>
 @endsection
