df8144b5fa58b605555f21594e28ce5cb907a12a
Yxq <15610538128@163.com>
2018-12-12 10:51:26 +0800

    新插件

--- webpack.mix.js
+++ webpack.mix.js
@@ -34,6 +34,7 @@ mix.copyDirectory('resources/assets/static_m/js/raty', 'public/static_m/js/raty'
 mix.copyDirectory('resources/assets/static_m/js/jquery.countdown-2.2.0', 'public/static_m/js/jquery.countdown-2.2.0');
 mix.copyDirectory('resources/assets/static_m/js/animate', 'public/static_m/js/animate');
 mix.copyDirectory('resources/assets/static_m/js/dropload', 'public/static_m/js/dropload');
+mix.copyDirectory('resources/assets/static_m/js/clipboard', 'public/static_m/js/clipboard');
 
 
 // dev
