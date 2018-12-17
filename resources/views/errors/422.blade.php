99d017a10e03cf7649d4a1e881e2ee130802a639
elijah <1198331919@qq.com>
2018-12-17 17:43:06 +0800

    A Better Error Page With Redirection to Root Page

--- resources/views/errors/422.blade.php
+++ resources/views/errors/422.blade.php
@@ -15,11 +15,12 @@
     <br/>
     <a href="{{back()->getTargetUrl()}}"><<< 返回上一页 </a>--}}
     <div class="error_box">
-    	<img src="{{ asset('defaults/default_mobile_404.png') }}">
-    	<p class="content">网络请求失败</p>
-    	<p class="content">请检查您的网络</p>
-    	<p class="btnBox">
-    		<a href="{{back()->getTargetUrl()}}">返回上一页</a>
-    	</p>
+        <img src="{{ asset('defaults/default_mobile_404.png') }}">
+        <p class="content">网络请求失败</p>
+        <p class="content">请检查您的网络</p>
+        <p class="btnBox">
+            {{--<a href="{{back()->getTargetUrl()}}">返回上一页</a>--}}
+            <a href="{{ route('root') }}">返回首页</a>
+        </p>
     </div>
 @stop
