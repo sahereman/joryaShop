06da0a34aa2d83e935fb7b95017c5338d42ea7fa
elijah <1198331919@qq.com>
2018-12-17 16:51:08 +0800

    default qr-code.png @ config

--- database/seeds/ConfigsSeeder.php
+++ database/seeds/ConfigsSeeder.php
@@ -41,8 +41,8 @@ class ConfigsSeeder extends Seeder
                         ['name' => '增值电信业务经营许可证', 'code' => 'certificate_no', 'type' => "text", 'sort' => 40, 'value' => '鲁B2-20110000'],
                         // ICP （电信与信息服务业务经营许可证）: Internet Content Provider
                         ['name' => '鲁公网备案号', 'code' => 'icp_no', 'type' => "text", 'sort' => 50, 'value' => '33010600000000'],
-                        ['name' => '关注公众号', 'code' => 'wechat_mp_qr_code', 'type' => "image", 'sort' => 60, 'value' => '', 'help' => '网站首页的关注公众号二维码图片',],
-                        ['name' => '手机逛商城', 'code' => 'mobile_website_qr_code', 'type' => "image", 'sort' => 70, 'value' => '', 'help' => '网站首页的手机逛商城二维码图片',],
+                        ['name' => '关注公众号', 'code' => 'wechat_mp_qr_code', 'type' => "image", 'sort' => 60, 'value' => '/defaults/wechat_mp_qr_code.png', 'help' => '网站首页的关注公众号二维码图片',],
+                        ['name' => '手机逛商城', 'code' => 'mobile_website_qr_code', 'type' => "image", 'sort' => 70, 'value' => '/defaults/mobile_website_qr_code.png', 'help' => '网站首页的手机逛商城二维码图片',],
                     ]
             ],
 
@@ -91,6 +91,9 @@ class ConfigsSeeder extends Seeder
             $group = Config::create(array_except($item, 'configs'));
 
             foreach ($item['configs'] as $config) {
+                if (in_array($config['code'], ['wechat_mp_qr_code', 'mobile_website_qr_code'])) {
+                    $config['value'] = asset($config['value']);
+                }
                 Config::create(array_merge($config, ['parent_id' => $group->id]));
             }
         }
