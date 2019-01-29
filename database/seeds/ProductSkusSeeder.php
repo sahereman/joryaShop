8385a84fbe55b230caaf44ee0c18919b112c4473
elijah <1198331919@qq.com>
2019-01-29 09:51:43 +0800

    A better product_skus_seeder

--- database/seeds/ProductSkusSeeder.php
+++ database/seeds/ProductSkusSeeder.php
@@ -29,8 +29,50 @@ class ProductSkusSeeder extends Seeder
             ]);
             factory(ProductSku::class)->create([
                 'product_id' => $product->id,
+                'name_en' => 'A - 2',
+                'name_zh' => 'A - 2 型',
+
+                // 2019-01-22
+                'base_size_en' => '10 * 10 (in)',
+                'base_size_zh' => '25 * 25 (cm)',
+                'hair_colour_en' => 'light grey',
+                'hair_colour_zh' => '浅灰色',
+                'hair_density_en' => '70%',
+                'hair_density_zh' => '70%',
+                // 2019-01-22
+            ]);
+            factory(ProductSku::class)->create([
+                'product_id' => $product->id,
+                'name_en' => 'A - 3',
+                'name_zh' => 'A - 3 型',
+
+                // 2019-01-22
+                'base_size_en' => '10 * 12 (in)',
+                'base_size_zh' => '25 * 30 (cm)',
+                'hair_colour_en' => 'light grey',
+                'hair_colour_zh' => '浅灰色',
+                'hair_density_en' => '70%',
+                'hair_density_zh' => '70%',
+                // 2019-01-22
+            ]);
+            factory(ProductSku::class)->create([
+                'product_id' => $product->id,
                 'name_en' => 'B - 1',
                 'name_zh' => 'B - 1 型',
+
+                // 2019-01-22
+                'base_size_en' => '10 * 10 (in)',
+                'base_size_zh' => '25 * 25 (cm)',
+                'hair_colour_en' => 'light grey',
+                'hair_colour_zh' => '浅灰色',
+                'hair_density_en' => '80%',
+                'hair_density_zh' => '80%',
+                // 2019-01-22
+            ]);
+            factory(ProductSku::class)->create([
+                'product_id' => $product->id,
+                'name_en' => 'B - 2',
+                'name_zh' => 'B - 2 型',
 
                 // 2019-01-22
                 'base_size_en' => '10 * 10 (in)',
@@ -43,8 +85,50 @@ class ProductSkusSeeder extends Seeder
             ]);
             factory(ProductSku::class)->create([
                 'product_id' => $product->id,
+                'name_en' => 'B - 3',
+                'name_zh' => 'B - 3 型',
+
+                // 2019-01-22
+                'base_size_en' => '10 * 10 (in)',
+                'base_size_zh' => '25 * 25 (cm)',
+                'hair_colour_en' => 'dark black',
+                'hair_colour_zh' => '深黑色',
+                'hair_density_en' => '80%',
+                'hair_density_zh' => '80%',
+                // 2019-01-22
+            ]);
+            factory(ProductSku::class)->create([
+                'product_id' => $product->id,
                 'name_en' => 'C - 1',
                 'name_zh' => 'C - 1 型',
+
+                // 2019-01-22
+                'base_size_en' => '10 * 12 (in)',
+                'base_size_zh' => '25 * 30 (cm)',
+                'hair_colour_en' => 'dark black',
+                'hair_colour_zh' => '深黑色',
+                'hair_density_en' => '70%',
+                'hair_density_zh' => '70%',
+                // 2019-01-22
+            ]);
+            factory(ProductSku::class)->create([
+                'product_id' => $product->id,
+                'name_en' => 'C - 2',
+                'name_zh' => 'C - 2 型',
+
+                // 2019-01-22
+                'base_size_en' => '10 * 12 (in)',
+                'base_size_zh' => '25 * 30 (cm)',
+                'hair_colour_en' => 'dark black',
+                'hair_colour_zh' => '深黑色',
+                'hair_density_en' => '80%',
+                'hair_density_zh' => '80%',
+                // 2019-01-22
+            ]);
+            factory(ProductSku::class)->create([
+                'product_id' => $product->id,
+                'name_en' => 'C - 3',
+                'name_zh' => 'C - 3 型',
 
                 // 2019-01-22
                 'base_size_en' => '10 * 12 (in)',
