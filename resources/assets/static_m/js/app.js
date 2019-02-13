d2ae7521d76f48ebf116079a0f8369c2a7607d64
Yxq <15610538128@163.com>
2019-02-13 17:50:42 +0800

    解决ios10及以上Safari无法禁止缩放的问题

--- resources/assets/static_m/js/app.js
+++ resources/assets/static_m/js/app.js
@@ -131,5 +131,27 @@ window.onload = function(){
 	setInterval(function(){
 		flag = true;
 	},100);
-}
+	
+	//解决ios10及以上Safari无法禁止缩放的问题
+	// 阻止双击放大
+    var lastTouchEnd = 0;
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    });
+    document.addEventListener('touchend', function(event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false);
 
+    // 阻止双指放大
+    document.addEventListener('gesturestart', function(event) {
+        event.preventDefault();
+    });
+	
+	
+}
