00ae730ddcd836d61ca34f8db205ba42ae32d745
Miracle <120007700@qq.com>
2019-05-10 17:00:38 +0800

    固定的导航栏目

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,35 +1,35 @@
 <nav class="navbar navbar-default">
     <div class="navbar-top">
         <div class="m-wrapper">
-           <div class="navbar-top-left pull-left">
+            <div class="navbar-top-left pull-left">
                 <ul>
                     <li>
                         <span>Tel: 400-100-5678</span>
                     </li>
-                     {{--<li>
-                        <span>@lang('app.switch language')：</span>
-                    </li>
-                    <li class="dropdown">
-                        <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">
-                            <span>{{ App::getLocale() == 'en' ? 'English' : '中文' }}</span>
-                            <img src="{{ asset('img/header/down_arrow.png') }}">
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dLabel">
-                            <li>
-                                <a href="{{ route('locale.update', ['locale' => 'zh-CN']) }}">
-                                    <img src="{{ asset('img/header/cn_flag.png') }}">
-                                    <span>中文</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('locale.update', ['locale' => 'en']) }}">
-                                    <img src="{{ asset('img/header/en_flag.png') }}">
-                                    <span>English</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>--}}
+                    {{--<li>
+                       <span>@lang('app.switch language')：</span>
+                   </li>
+                   <li class="dropdown">
+                       <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true"
+                               aria-expanded="false">
+                           <span>{{ App::getLocale() == 'en' ? 'English' : '中文' }}</span>
+                           <img src="{{ asset('img/header/down_arrow.png') }}">
+                       </button>
+                       <ul class="dropdown-menu" aria-labelledby="dLabel">
+                           <li>
+                               <a href="{{ route('locale.update', ['locale' => 'zh-CN']) }}">
+                                   <img src="{{ asset('img/header/cn_flag.png') }}">
+                                   <span>中文</span>
+                               </a>
+                           </li>
+                           <li>
+                               <a href="{{ route('locale.update', ['locale' => 'en']) }}">
+                                   <img src="{{ asset('img/header/en_flag.png') }}">
+                                   <span>English</span>
+                               </a>
+                           </li>
+                       </ul>
+                   </li>--}}
                 </ul>
             </div>
             <div class="navbar-top-right pull-right">
@@ -121,6 +121,81 @@
                             </li>
                         @endif
                     @endforeach
+
+                    {{--固定的导航栏目--}}
+                    <li class="first_menu">
+                        <a href="{{url('articles/starting_a_purchase')}}">STARTING A PURCHASE</a>
+                        <div class="nav-panel-dropdown">
+                            <ul>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'how_to_send_in_samples']) }}">
+                                        <span>How To send in samples</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'existing_client_reorder']) }}">
+                                        <span>Existing client reorder</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'hair_duplication']) }}">
+                                        <span>Hair duplication</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'repair_old_system']) }}">
+                                        <span>Repair old system</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{url('storage/LYRICAL%20HAIR%20ORDER%20FORM.doc')}}">
+                                        <span>Download the Custom Order Form</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'order_tools_reference']) }}">
+                                        <span>Order Tools Reference</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{url('storage/Download the Catalogue.docx')}}">
+                                        <span>Download Catalogue</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'return_policy']) }}">
+                                        <span>Return Policy</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'positive_comments']) }}">
+                                        <span>Positive comments</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'common_order_mistakes']) }}">
+                                        <span>Common Order Mistakes</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'hair_care']) }}">
+                                        <span>Hair care</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'send_in_templates_&_hair']) }}">
+                                        <span>Send in templates & Hair</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('articles.show', ['slug' => 'samples']) }}">
+                                        <span>Samples</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
                 </ul>
                 <div class="pull-right header-search">
                     <ul>
@@ -130,7 +205,8 @@
                             </a>
                         </li>
                         <li class="show_search">
-                            <input type="search" data-url="{{ route('products.search_hint') }}" class="selectInput_header" placeholder="@lang('app.Please enter the item you are searching for')">
+                            <input type="search" data-url="{{ route('products.search_hint') }}" class="selectInput_header"
+                                   placeholder="@lang('app.Please enter the item you are searching for')">
                             <a class="search_btn" href="javascript:void(0);">
                                 <img src="{{ asset('img/search_magnifier.png') }}">
                             </a>
