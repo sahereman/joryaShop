9c12b5386a8c7c76a93149bc459af8e99b5f7e17
Yxq <15610538128@163.com>
2019-05-13 14:34:07 +0800

    富文本样式测试

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,8 +6,32 @@
 
     <div class="common_articles products-search-level">
         <div class="m-wrapper">
-            {!! App::isLocale('zh-CN') ? $article->content_zh : $article->content_en !!}
+            <iframe name="cmsCon" id="cmsCon" class="cmsCon" frameborder="0" width="100%" scrolling="no" height="auto">
+                {!! App::isLocale('zh-CN') ? $article->content_zh : $article->content_en !!}
+            </iframe>
         </div>
     </div>
 
+@endsection
+@section('scriptsAfterJs')
+    <script type="text/javascript">
+//  $('#cmsCon').contents().find('body').html('展示内容');
+    autoHeight();  //动态调整高度
+    var count = 0;
+            var autoSet = window.setInterval('autoHeight()',500);
+            function autoHeight(){
+                var mainheight;
+                count++;
+                if(count == 1){
+                    mainheight = $('.cmsCon').contents().find("body").height()+50;
+                }else{
+                    mainheight = $('.cmsCon').contents().find("body").height()+24;
+                }
+                $('.cmsCon').height(mainheight);
+                if(count == 5){
+                    window.clearInterval(autoSet);
+                }
+                
+            }
+    </script>
 @endsection
