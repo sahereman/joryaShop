a21a5b93cb7f37366b1e434947861fad78b6d6f8
Yxq <15610538128@163.com>
2019-05-14 09:52:28 +0800

    富文本微调4

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,14 +6,25 @@
 
     <div class="common_articles products-search-level">
         <div class="m-wrapper">
-            <iframe name="cmsCon" id="cmsCon" class="cmsCon" frameborder="0" width="100%" scrolling="no" height="auto"></iframe>
+            <div class="aaaa" style="display: none;">
+                {!! $article->content_en !!}
+            </div>
+            <iframe name="cmsCon" id="cmsCon" class="cmsCon" frameborder="0" width="100%" scrolling="no" height="auto">
+                
+            </iframe>
         </div>
     </div>
 
 @endsection
 @section('scriptsAfterJs')
     <script type="text/javascript">
-    $('#cmsCon').contents().find('body').html('{!! App::isLocale('zh-CN') ? $article->content_zh : $article->content_en !!}');
+        
+
+
+//      var aaa = eval("{!! App::isLocale('zh-CN') ? $article->content_zh : $article->content_en !!}");
+//      var aaa = eval("{!! $article->content_en !!}");
+        var aaa = $('.aaa').html();
+    $('#cmsCon').contents().find('body').html(aaa);
     autoHeight();  //动态调整高度
     var count = 0;
             var autoSet = window.setInterval('autoHeight()',500);
