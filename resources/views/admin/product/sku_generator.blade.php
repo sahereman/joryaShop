d9121dd4c3c56bc673316bf6b3a22ed4db610f99
Yxq <15610538128@163.com>
2019-05-15 17:39:24 +0800

    sku生成器2

--- resources/views/admin/product/sku_generator.blade.php
+++ resources/views/admin/product/sku_generator.blade.php
@@ -13,7 +13,7 @@
                 <tr>
                     <td>
                         <div class="input-group">
-                          <input type="text" class="form-control" name="BaseSize[]">
+                          <input type="text" class="form-control" name="base_size[]">
                           <span class="input-group-btn">
                             <button class="btn btn-danger" type="button" onclick="delCol()">
                                 <span class="glyphicon glyphicon-remove"></span>
@@ -23,7 +23,7 @@
                     </td>
                     <td>
                         <div class="input-group">
-                            <input type="text" name="HairColour[]" class="form-control">
+                            <input type="text" name="hair_colour[]" class="form-control">
                             <span class="input-group-btn">
                                 <button class="btn btn-danger" type="button" onclick="delCol()">
                                     <span class="glyphicon glyphicon-remove"></span>
@@ -33,7 +33,7 @@
                     </td>
                     <td>
                         <div class="input-group">
-                            <input type="text" name="HairDensity[]" class="form-control">
+                            <input type="text" name="hair_density[]" class="form-control">
                             <span class="input-group-btn">
                                 <button class="btn btn-danger" type="button" onclick="delCol()">
                                     <span class="glyphicon glyphicon-remove"></span>
@@ -41,16 +41,16 @@
                             </span>
                         </div>
                     </td>
-                </tr>
+                </tr>                
                 <tr>
                     <td>
-                        <button type="button" atName="BaseSize[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
+                        <button type="button" atName="base_size[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
                     </td>
                     <td>
-                        <button type="button" atName="HairColour[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
+                        <button type="button" atName="hair_colour[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
                     </td>
                     <td>
-                        <button type="button" atName="HairDensity[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
+                        <button type="button" atName="hair_density[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
                     </td>
                 </tr>
             </table>
@@ -69,6 +69,8 @@
                     "<td></td>"+
                     "</tr>"
     var inp_name,
+        insert_num,  //列下标值
+        trinsert_num, //行下标值
         addColCom = "<div class='input-group'>"+
                     "<input type='text' name='"+ inp_name +"' class='form-control'>"+
                     "<span class='input-group-btn'>"+
@@ -79,31 +81,69 @@
                     "</div>"
     function addCol(){
         var _$this = $(event.target),
-            insert_num = _$this.parent("td").index();
-            console.log(adtive_row_col);
+            _that =$(active_row_tds).find("td")[insert_num];
+        insert_num = _$this.parent("td").index();
+        //当倒数第二行的表格填满后增加新的一行
         if(table_row_th == adtive_row_col) {
             _$this.parents("tr").before(addRowCom);
-            active_row = $(".table tr").length-2;  //更新数值
-            active_row_tds = $(".table tr")[active_row]; //更新数值
-            adtive_row_col = $(active_row_tds).find(".input-group").length; //更新数值
+            updateNum();
             inp_name = _$this.attr("atName");
-            var _that =$(active_row_tds).find("td")[insert_num];
-            _that.innerHTML=addColCom;
+            _that =$(active_row_tds).find("td")[insert_num];
+            if($(_that).find(".input-group").length == 0) {
+              _that.innerHTML=addColCom;   
+            }
         }else {
-            active_row = $(".table tr").length-2;  //更新数值
-            active_row_tds = $(".table tr")[active_row]; //更新数值
-            adtive_row_col = $(active_row_tds).find(".input-group").length; //更新数值
+            updateNum();
             inp_name = _$this.attr("atName");
-            var _that =$(active_row_tds).find("td")[insert_num];
-            _that.innerHTML=addColCom;
+            _that =$(active_row_tds).find("td")[insert_num];
+            //获取第一个存在td为空的tr的下标值
+            forrows(insert_num);
+            //根据下标值查找对应的tr
+            var missRow = $(".table").find("tr")[trinsert_num],
+            //根据点击按钮的下标值查找第一个存在td为空的tr中的对应下标值的td
+                judgeCol = $(missRow).find("td")[insert_num];   
+            //判断根据点击事件传入的下标值的为空tr中的对应的td是否为空,
+            //如果innerhtml为空时向其中插入模板内容
+            //如果tr中存在内容为空的td，但是点击事件传入的下表值对应的td存在，则tr的下标值加一
+            var lastNumTr = $(".table").find("tr").length - 1;
+            if(trinsert_num > lastNumTr-1) {
+                _$this.parents("tr").before(addRowCom);
+                updateNum();
+                inp_name = _$this.attr("atName");
+                 _that =$(active_row_tds).find("td")[insert_num];
+                if($(_that).find(".input-group").length == 0) {
+                  _that.innerHTML=addColCom;   
+                }
+            }else {
+                judgeCol.innerHTML=addColCom;
+            }
             if(insert_num == table_row_th-1) {
                 adtive_row_col +=1;
             }
         }
-        
+    }
+    function updateNum(){
+        active_row = $(".table tr").length-2;  //更新数值
+        active_row_tds = $(".table tr")[active_row]; //更新数值
+        adtive_row_col = $(active_row_tds).find(".input-group").length; //更新数值
     }
     function delCol(){
         var _$this = $(event.target);
         _$this.parents(".input-group").html("");
+    }
+    //遍历查找td内容为空的行
+    //查找第一个td存在为空的情况
+    function forrows(tdIndex){
+        var trArr = $(".table").find("tr");
+        var _that;
+        for(var i = 1;i<=trArr.length-1;i++){
+            if($(trArr[i]).find(".input-group").length< table_row_th) {
+                _that = $(trArr[i]).find("td")[tdIndex];
+                if($(_that).find(".input-group").length == 0){
+                    trinsert_num = i;   
+                    return
+                }
+            }
+        }
     }
 </script>
