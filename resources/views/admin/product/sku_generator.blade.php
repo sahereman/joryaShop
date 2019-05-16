f5bd10b0df379d8d2d1375f4ad93db308dca2f69
Yxq <15610538128@163.com>
2019-05-16 16:02:42 +0800

    sku生成器3

--- resources/views/admin/product/sku_generator.blade.php
+++ resources/views/admin/product/sku_generator.blade.php
@@ -1,149 +1,181 @@
 <div class="box">
-	<form role="form">
+	<form role="form" 
+	      method="POST" 
+	      action=""
+          enctype="multipart/form-data"
+    >
+    {{ csrf_field() }}
+    <input type="hidden" name="data_json" value="">
 	    <div class="box-header">
-	        <button type="button" class="btn-group pull-right btn btn-primary">保存</button>
+	        <button type="button" class="btn-group pull-right btn btn-primary" id="submit_btn">保存</button>
 	    </div>
 	    <div class="box-body">
-	        <table class="table">
-                <tr>
-                    <th>Base Size</th>
-                    <th>Hair Colour</th>
-                    <th>Hair Density</th>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="input-group">
-                          <input type="text" class="form-control" name="base_size[]">
-                          <span class="input-group-btn">
-                            <button class="btn btn-danger" type="button" onclick="delCol()">
-                                <span class="glyphicon glyphicon-remove"></span>
-                            </button>
-                          </span>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="input-group">
-                            <input type="text" name="hair_colour[]" class="form-control">
-                            <span class="input-group-btn">
-                                <button class="btn btn-danger" type="button" onclick="delCol()">
-                                    <span class="glyphicon glyphicon-remove"></span>
-                                </button>
-                            </span>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="input-group">
-                            <input type="text" name="hair_density[]" class="form-control">
-                            <span class="input-group-btn">
-                                <button class="btn btn-danger" type="button" onclick="delCol()">
-                                    <span class="glyphicon glyphicon-remove"></span>
-                                </button>
-                            </span>
-                        </div>
-                    </td>
-                </tr>                
-                <tr>
-                    <td>
-                        <button type="button" atName="base_size[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
-                    </td>
-                    <td>
-                        <button type="button" atName="hair_colour[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
-                    </td>
-                    <td>
-                        <button type="button" atName="hair_density[]" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
-                    </td>
-                </tr>
-            </table>
+	        <div class="container-fluid">
+              <div class="row">
+                  <!--col-md-4这个class值不是固定的。要根据不同的数目的表格来进行区分，总数为12，现在有3类每一类占4分，-->
+                  <div class="col-md-4">
+                  	<table class="table photo_tab" arr_nmae = 'color'>
+                        <tr>
+                            <th>Hair Colour</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="input-group">
+                                    <span class="input-group-btn pic_btn" style="overflow: hidden;">
+                                        <img src="{{ asset('img/pic_upload.png') }}" 
+                                             style="height: 34px;border: 1px solid #ccc;padding: 2px;">
+                                        <input type="file" name="image" 
+                                               style="opacity: 0;position: absolute;top: 0;width: 100%;height: 100%;" 
+                                               data-url="{{ route('image.upload') }}" 
+                                               onchange="imgChange(this)"
+                                        >
+                                    </span>
+                                    <input type="text" name="hair_colour" data_path = '' class="form-control">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-danger" type="button" onclick="delCol()">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button type="button" atName="hair_colour" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
+                            </td>
+                        </tr>                
+                    </table>
+                  </div>
+                  <div class="col-md-4">
+                    <table class="table" arr_nmae = 'size'>
+                        <tr>
+                            <th>Hair Size</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="input-group">
+                                    <input type="text" name="base_size" class="form-control">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-danger" type="button" onclick="delCol()">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button type="button" atName="base_size" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
+                            </td>
+                        </tr>                
+                    </table>
+                  </div>
+                  <div class="col-md-4">
+                    <table class="table" arr_nmae = 'density'>
+                        <tr>
+                            <th>Hair Density</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="input-group">
+                                    <input type="text" name="hair_density" class="form-control">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-danger" type="button" onclick="delCol()">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button type="button" atName="hair_density" class="btn-group  btn btn-primary" onclick="addCol()">增加</button>
+                            </td>
+                        </tr>                
+                    </table>
+                  </div>
+              </div>
+            </div>
 	    </div>
 	</form>
 </div>
 <script type="text/javascript">
-    var active_row = $(".table tr").length-2,
-        active_row_tds = $(".table tr")[active_row], //用于获取活跃行及修改行的数目
-        table_row_tds = $(".table tr")[0],  //表头用于获取th的数目
-        table_row_th = $(table_row_tds).find("th").length;    //th的数目
-        adtive_row_col = $(active_row_tds).find(".input-group").length; //新增行既修改行的列数
-    var addRowCom = "<tr>"+
-                    "<td></td>"+
-                    "<td></td>"+
-                    "<td></td>"+
-                    "</tr>"
-    var inp_name,
-        insert_num,  //列下标值
-        trinsert_num, //行下标值
-        addColCom = "<div class='input-group'>"+
-                    "<input type='text' name='"+ inp_name +"' class='form-control'>"+
-                    "<span class='input-group-btn'>"+
-                    "<button class='btn btn-danger' type='button' onclick='delCol()'>"+
-                    "<span class='glyphicon glyphicon-remove'></span>"+
-                    "</button>"+
-                    "</span>"+
-                    "</div>"
     function addCol(){
-        var _$this = $(event.target),
-            _that =$(active_row_tds).find("td")[insert_num];
-        insert_num = _$this.parent("td").index();
-        //当倒数第二行的表格填满后增加新的一行
-        if(table_row_th == adtive_row_col) {
-            _$this.parents("tr").before(addRowCom);
-            updateNum();
-            inp_name = _$this.attr("atName");
-            _that =$(active_row_tds).find("td")[insert_num];
-            if($(_that).find(".input-group").length == 0) {
-              _that.innerHTML=addColCom;   
-            }
-        }else {
-            updateNum();
-            inp_name = _$this.attr("atName");
-            _that =$(active_row_tds).find("td")[insert_num];
-            //获取第一个存在td为空的tr的下标值
-            forrows(insert_num);
-            //根据下标值查找对应的tr
-            var missRow = $(".table").find("tr")[trinsert_num],
-            //根据点击按钮的下标值查找第一个存在td为空的tr中的对应下标值的td
-                judgeCol = $(missRow).find("td")[insert_num];   
-            //判断根据点击事件传入的下标值的为空tr中的对应的td是否为空,
-            //如果innerhtml为空时向其中插入模板内容
-            //如果tr中存在内容为空的td，但是点击事件传入的下表值对应的td存在，则tr的下标值加一
-            var lastNumTr = $(".table").find("tr").length - 1;
-            if(trinsert_num > lastNumTr-1) {
-                _$this.parents("tr").before(addRowCom);
-                updateNum();
-                inp_name = _$this.attr("atName");
-                 _that =$(active_row_tds).find("td")[insert_num];
-                if($(_that).find(".input-group").length == 0) {
-                  _that.innerHTML=addColCom;   
-                }
-            }else {
-                judgeCol.innerHTML=addColCom;
-            }
-            if(insert_num == table_row_th-1) {
-                adtive_row_col +=1;
-            }
-        }
-    }
-    function updateNum(){
-        active_row = $(".table tr").length-2;  //更新数值
-        active_row_tds = $(".table tr")[active_row]; //更新数值
-        adtive_row_col = $(active_row_tds).find(".input-group").length; //更新数值
+        var _$this = $(event.target);
+        var addRowCom = "<tr>"+ _$this.parents("table").find("tr")[1].innerHTML +"</tr>";
+        _$this.parents("tr").before(addRowCom);
+        _$this.parents("tr").prev().find("img").attr('src',"{{ asset('img/pic_upload.png') }}");
+        _$this.parents("tr").prev().find("input").val("");
     }
     function delCol(){
         var _$this = $(event.target);
-        _$this.parents(".input-group").html("");
-    }
-    //遍历查找td内容为空的行
-    //查找第一个td存在为空的情况
-    function forrows(tdIndex){
-        var trArr = $(".table").find("tr");
-        var _that;
-        for(var i = 1;i<=trArr.length-1;i++){
-            if($(trArr[i]).find(".input-group").length< table_row_th) {
-                _that = $(trArr[i]).find("td")[tdIndex];
-                if($(_that).find(".input-group").length == 0){
-                    trinsert_num = i;   
-                    return
-                }
-            }
+        if(_$this.parents("table").find("tr").length != 3) {
+            _$this.parents("tr").remove();   
         }
     }
+    //图片上传
+    function imgChange(obj){
+        var filePath = $(obj).val();
+        var url = $(obj).attr("data-url");
+//      if($(obj).parents("tr").find("input[type='text']").val() == "" ){
+//          alert("请完善对应信息才可以上传图片!")
+//      }else {
+            UpLoadImg(obj, url);   
+//      }
+    }
+    function UpLoadImg(obj, url) {
+        var formData = new FormData();
+        formData.append('image', $(obj)[0].files[0]);
+        formData.append('_token', "{{ csrf_token() }}");
+        $.ajax({
+            url: url,
+            data: formData,
+            dataType: 'json',
+            cache: false,
+            contentType: false,//必须false才会避开jQuery对 formdata 的默认处理 XMLHttpRequest会对 formdata 进行正确的处理  
+            processData: false,//必须false才会自动加上正确的Content-Type
+            type: 'post',
+            success: function (data) {
+                $(obj).parents("span").find("img").attr("src",data.preview);
+                $(obj).parents("tr").find("input[type=text]").attr("data_path",data.path);
+            }, 
+            error: function (e) {
+                console.log(e)
+            }
+        });
+    } 
+    //去重
+    function unique(arr, type) {
+        const res = new Map();
+        return arr.filter((a) => !res.has(a[type]) && res.set(a[type], 1));
+    }
+    //表单提交
+    $('#submit_btn').on("click",function(){
+        var json_str = new Object(),
+            totalArr = [];
+        var totalTab = $(".box-body").find("table");
+        var photo_tab = $(".photo_tab").find("tr");
+        if($(".table").find("input[type=text]").val()==""){
+            alert("请完善信息!");
+            return;
+        }
+        for(var i = 0;i <= totalTab.length-1;i++) {
+            var kyeNmae = $(totalTab[i]).attr("arr_nmae");
+            var totalTr = $(totalTab[i]).find("tr");
+            totalArr = [];
+            for(var iteam = 1; iteam<=totalTr.length-2; iteam++){
+                if($(totalTab[i]).hasClass("photo_tab")){
+                    totalArr.push({"data":$(totalTr[iteam]).find("input[type=text]").val(),"photo":$(totalTr[iteam]).find("input[type=text]").attr("data_path")});   
+                }else {
+                    totalArr.push({"data":$(totalTr[iteam]).find("input[type=text]").val()});
+                }
+            }
+            totalArr = unique(totalArr,"data");
+            json_str[kyeNmae] = totalArr;
+        }
+        $("input[name='data_json']").val(JSON.stringify(json_str));
+        setTimeout(function(){
+            $("form").submit();
+        },500)
+    })
 </script>
