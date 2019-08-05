eb705abf99870e8f7524966e4adb38f0025f9f98
Yxq <15610538128@163.com>
2019-08-05 15:54:25 +0800

    sku列表按钮大小调整

--- resources/views/admin/product/sku_editor.blade.php
+++ resources/views/admin/product/sku_editor.blade.php
@@ -183,10 +183,10 @@
                     <tr>
                         <td>
                             <img src="{{ $sku->photo_url }}" style="min-width:60px;min-height:60px;max-width:60px;max-height:200px" class="img img-thumbnail">
-                            <div class="changeImgArea" style="position: relative;width: 40px;height:40px;overflow: hidden;margin-top: 5px;">
+                            <div class="changeImgArea" style="position: relative;width: 40px;height:25px;overflow: hidden;margin-top: 5px;">
                                 <input type="file" id="skus[{{ $sku->id }}][photo]" name="skus[{{ $sku->id }}][photo]" value="{{ $sku->photo }}"
                                        style="width: 75px;position: absolute;top: 0;z-index: 5;opacity: 0">
-                                <button type="button" class="btn-group btn btn-primary" aria-label="Left Align" style="position: absolute;top: 0;">
+                                <button type="button" class="btn-group btn btn-primary btn-xs" aria-label="Left Align" style="position: absolute;top: 0;">
                                     <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
                                 </button>
                             </div>
