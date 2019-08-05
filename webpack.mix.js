9975acc237cc7757144935fc457ca93bcb359cb5
Yxq <15610538128@163.com>
2019-08-05 11:01:13 +0800

    js生成与引入

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,20 +24,21 @@ mix.copyDirectory('resources/assets/js/swiper', 'public/js/swiper');
 mix.copyDirectory('resources/assets/js/jquery.validate.min.js', 'public/js/jquery.validate.min.js');
 mix.copyDirectory('resources/assets/js/layer', 'public/js/layer');
 mix.copyDirectory('resources/assets/js/shareJS', 'public/js/shareJS');
+mix.copyDirectory('resources/assets/js/lord', 'public/js/lord');
 
 //mix.copyDirectory('resources/assets/js/main.js', 'public/js/');
 
 // Mobile 站
-mix.js('resources/assets/static_m/js/app.js', 'public/static_m/js')
-    .sass('resources/assets/static_m/sass/app.scss', 'public/static_m/css');
-
-mix.copyDirectory('resources/assets/static_m/img', 'public/static_m/img');
-mix.copyDirectory('resources/assets/static_m/js/layer_mobile', 'public/static_m/js/layer_mobile');
-mix.copyDirectory('resources/assets/static_m/js/raty', 'public/static_m/js/raty');
-mix.copyDirectory('resources/assets/static_m/js/jquery.countdown-2.2.0', 'public/static_m/js/jquery.countdown-2.2.0');
-mix.copyDirectory('resources/assets/static_m/js/animate', 'public/static_m/js/animate');
-mix.copyDirectory('resources/assets/static_m/js/dropload', 'public/static_m/js/dropload');
-mix.copyDirectory('resources/assets/static_m/js/clipboard', 'public/static_m/js/clipboard');
+// mix.js('resources/assets/static_m/js/app.js', 'public/static_m/js')
+//     .sass('resources/assets/static_m/sass/app.scss', 'public/static_m/css');
+//
+// mix.copyDirectory('resources/assets/static_m/img', 'public/static_m/img');
+// mix.copyDirectory('resources/assets/static_m/js/layer_mobile', 'public/static_m/js/layer_mobile');
+// mix.copyDirectory('resources/assets/static_m/js/raty', 'public/static_m/js/raty');
+// mix.copyDirectory('resources/assets/static_m/js/jquery.countdown-2.2.0', 'public/static_m/js/jquery.countdown-2.2.0');
+// mix.copyDirectory('resources/assets/static_m/js/animate', 'public/static_m/js/animate');
+// mix.copyDirectory('resources/assets/static_m/js/dropload', 'public/static_m/js/dropload');
+// mix.copyDirectory('resources/assets/static_m/js/clipboard', 'public/static_m/js/clipboard');
 
 
 // dev
