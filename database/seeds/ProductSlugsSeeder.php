682a35c64aa8fe47e59b92061d8cdd2f792b9be0
Miracle <120007700@qq.com>
2019-08-09 16:53:38 +0800

    categories  slug seeder

--- database/seeds/ProductSlugsSeeder.php
+++ database/seeds/ProductSlugsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Database\Seeder;
 
 class ProductSlugsSeeder extends Seeder
@@ -14,6 +15,10 @@ class ProductSlugsSeeder extends Seeder
     {
         Product::all()->each(function (Product $product) {
             $product->save();
+        });
+
+        ProductCategory::all()->each(function (ProductCategory $productCategory) {
+            $productCategory->save();
         });
     }
 }
