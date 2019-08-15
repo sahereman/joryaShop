8ab4a0544762f1c185888969113d3598acf36942
Miracle <120007700@qq.com>
2019-08-15 18:06:46 +0800

    article show update  2

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('keywords', $article->seo_keywords ? : \App\Models\Config::config('keywords'))
 @section('description', $article->seo_description ? : \App\Models\Config::config('description'))
-@section('title', $article->seo_title ? : $article->slug . ' - ' . \App\Models\Config::config('title'))
+@section('title', $article->seo_title ? : $article->name . ' - ' . \App\Models\Config::config('title'))
 @section('content')
     <div class="common_articles products-search-level">
         <div class="m-wrapper container">
